facaaa08c444ead46acea3e8c8c9dfc80ea9b69b
MarguJ <margulesjoa@gmail.com>
2026-06-17 08:36:24 -0300

    Primer endpoint modularizado

---
+++ backend/src/features/auth/authRepository.js
@@ -0,0 +1,16 @@
+import prisma from "../../config/prisma.js";
+
+export async function findByEmail(email) {
+  return prisma.users.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+      complete_name: true,
+      email: true,
+      role: true,
+      password: true,
+    },
+  });
+}
